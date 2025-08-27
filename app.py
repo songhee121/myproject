fd25dafee8dbc77eca2d3b7593e0d05856fa481d
songhee121 <hehe9331@gmail.com>
2025-08-27 17:43:49 +0900

    250827

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ page_1 = st.Page("p1.py", title="page 1", icon="📖")
 page_2 = st.Page("p2.py", title="개념 학습하기", icon="📖")
 page_3 = st.Page("p3.py", title="학습자료로 질문하기", icon="🙋")
 page_4 = st.Page("p4.py", title="녹음 내용 요약하기", icon="🎙️")
-page_5 = st.Page("p5.py", title="page 5", icon="📖")
+page_5 = st.Page("p5.py", title="녹음 및 파일로 저장하기", icon="🎤 ")
 page_8 = st.Page("p8.py", title="채팅하기", icon="💬")
 
 page = st.navigation([Main_page, page_1, page_2, page_3, page_4, page_5, page_8])
--- p5.py
+++ p5.py
@@ -1,6 +1,16 @@
+import os
 import streamlit as st
 from st_audiorec import st_audiorec
-from MYLLM import save_uploadedfile
+
+def save_uploadedfile(directory, data, filename, st):
+    # 1. 디렉토리가 없으면 생성
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    # 2. 파일 저장
+    with open(os.path.join(directory, filename), 'wb') as f:
+        f.write(data)
+    # 3. 저장 완료 메시지 출력
+    st.success(f'저장 완료: {directory}에 {filename}이 저장되었습니다.')
 
 st.markdown("🎤 녹음 및 파일로 저장하기")
 
@@ -8,14 +18,15 @@ st.markdown("🎤 녹음 및 파일로 저장하기")
 audio_data = st_audiorec()
 
 if audio_data:
-    st.audio(audio_data, format="audio/wav")
-    filename = st.text_input("저장할 파일 이름(확장자 제외): ", "my_record")
+    filename = st.text_input("저장할 파일 이름: ", "my_record")
+    filetype = st.selectbox("파일 타입 선택:", ["wav", "mp3", "m4a", "ogg", "webm"])
 
-    if filename:
-        save_uploadedfile("audio", audio_data, st)
+    if filename and filetype :
+        filename= f"{filename}.{filetype}"
+        save_uploadedfile("audio", audio_data, filename, st)
         st.download_button(
             label="파일 다운로드",
             data=audio_data,
             file_name=filename,
-            mime=f"audio/wav"
+            mime="audio/wav"
         )
