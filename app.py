1960283707a196a7587d9adf13aca31c594ceb66
songhee121 <hehe9331@gmail.com>
2025-08-28 15:56:33 +0900

    250828

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ import streamlit as st
 
 
 main_page = st.Page("main.py", title="학습 어시스트", icon="📚")
-page_1 = st.Page("page1.py", title="개념 학습하기", icon="📖")
-page_2 = st.Page("page2.py", title="학습자료로 질문하기", icon="🙋")
-page_3 = st.Page("page3.py", title="녹음 내용 요약하기", icon="🎙️")
-page_4 = st.Page("page4.py", title="설명문 보기", icon="📄️")
-page_5 = st.Page("page5.py", title="채팅하기", icon="💬")
-page_6 = st.Page("page6.py", title="찍어서 살펴보기", icon="📸")
-page_7 = st.Page("page7.py", title="스터디 플래너", icon="🧭")
-page_8 = st.Page("page8.py", title="달력에 메모하기", icon="📆️")
+page_1 = st.Page("p1.py", title="개념 학습하기", icon="📖")
+page_2 = st.Page("p2.py", title="학습자료로 질문하기", icon="🙋")
+page_3 = st.Page("p3.py", title="녹음 내용 요약하기", icon="🎙️")
+page_4 = st.Page("p4.py", title="설명문 보기", icon="📄️")
+page_5 = st.Page("p5.py", title="채팅하기", icon="💬")
+page_6 = st.Page("p6.py", title="찍어서 살펴보기", icon="📸")
+page_7 = st.Page("p7.py", title="스터디 플래너", icon="🧭")
+page_8 = st.Page("p8.py", title="달력에 메모하기", icon="📆️")
 
 
 
